730714d775a4ed1e60c18c87db2d759a7cb7bd9e
Andrew Hulbert <andrew.hulbert@ccri.com>
2016-04-04 10:57:41 -0400

    version

--- bin/test-geomesa.sh
+++ bin/test-geomesa.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-GM="/tmp/geomesa-tools-1.2.1-SNAPSHOT"
+GM="/tmp/geomesa-tools-1.2.2-SNAPSHOT"
+GMTMP="geomesa-test-tmp"
 geomesa="$GM/bin/geomesa"
 
 . "$HOME/dev/cloud-local/bin/config.sh"
@@ -37,35 +38,54 @@ $geomesa ingest -u root -p secret -c awstest -s geolife -C geolife s3n://ahulber
 # s3a
 $geomesa ingest -u root -p secret -c awstest -s geolife -C geolife s3a://ahulbert-test/geolife/Data/000/Trajectory/20081023025304.plt s3a://ahulbert-test/geolife/Data/000/Trajectory/20081024020959.plt
 
-if [[ -e /tmp/gm-e-test.csv ]]; then
-  rm /tmp/gm-e-test.csv
+if [[ -d "$GMTMP" ]]; then
+  rm "/tmp/${GMTMP}" -rf
 fi
-if [[ -e /tmp/gm-e-test.avro ]]; then
-  rm /tmp/gm-e-test.avro
-fi
-if [[ -e /tmp/gm-e-test-2.csv ]]; then
-  rm /tmp/gm-e-test-2.csv
-fi
-if [[ -e /tmp/gm-e-test-3.csv ]]; then
-  rm /tmp/gm-e-test-3.csv
-fi
-$geomesa export -u root -p secret -c test1 -f example-csv -F csv | sort > /tmp/gm-e-test.csv
-$geomesa export -u root -p secret -c test1 -f example-csv -F avro > /tmp/gm-e-test.avro
-$geomesa ingest -u root -p secret -c test1 -f reimport-avro /tmp/gm-e-test.avro
-hadoop fs -put /tmp/gm-e-test.avro
-$geomesa ingest -u root -p secret -c test1 -f reimport-avro2 hdfs://localhost:9000/user/$(whoami)/gm-e-test.avro
 
-$geomesa export -u root -p secret -c test1 -f reimport-avro -F csv | sort> /tmp/gm-e-test-2.csv
-$geomesa export -u root -p secret -c test1 -f reimport-avro2 -F csv | sort> /tmp/gm-e-test-3.csv
+t="/tmp/${GMTMP}"
+mkdir $t
 
-echo "diff /tmp/gm-e-test.csv /tmp/gm-e-test-2.csv"
-diff /tmp/gm-e-test.csv /tmp/gm-e-test-2.csv
-echo "Diff end"
+# Export some formats
+$geomesa export -u root -p secret -c test1 -f example-csv -F csv  > "$t/e.csv"
+$geomesa export -u root -p secret -c test1 -f example-csv -F avro > "$t/e.avro"
+$geomesa export -u root -p secret -c test1 -f example-csv -F tsv  > "$t/e.tsv"
 
-echo "diff /tmp/gm-e-test.csv /tmp/gm-e-test-3.csv"
-diff /tmp/gm-e-test.csv /tmp/gm-e-test-3.csv
-echo "Diff end"
+# Reingest automatically those formats both locally and via hdfs
+$geomesa ingest -u root -p secret -c test1 -f re-avro "$t/e.avro"
+$geomesa ingest -u root -p secret -c test1 -f re-csv  "$t/e.csv"
+$geomesa ingest -u root -p secret -c test1 -f re-tsv  "$t/e.tsv"
+
+hadoop fs -put "$t/e.avro"
+hadoop fs -put "$t/e.csv"
+hadoop fs -put "$t/e.tsv"
+
+$geomesa ingest -u root -p secret -c test1 -f re-avro-hdfs hdfs://localhost:9000/user/$(whoami)/e.avro
+$geomesa ingest -u root -p secret -c test1 -f re-csv-hdfs  hdfs://localhost:9000/user/$(whoami)/e.csv
+$geomesa ingest -u root -p secret -c test1 -f re-tsv-hdfs  hdfs://localhost:9000/user/$(whoami)/e.tsv
+
+# compare output of reimported tsv,csv,avro to standard export
+$geomesa export -u root -p secret -c test1 -f re-avro       -F csv  | sort >  "$t/re.avro.export"
+$geomesa export -u root -p secret -c test1 -f re-avro-hdfs  -F csv  | sort >  "$t/re.avro.hdfs.export"
+$geomesa export -u root -p secret -c test1 -f re-csv        -F csv  | sort >  "$t/re.csv.export"
+$geomesa export -u root -p secret -c test1 -f re-csv-hdfs   -F csv  | sort >  "$t/re.csv.hdfs.export"
+$geomesa export -u root -p secret -c test1 -f re-tsv        -F csv  | sort >  "$t/re.tsv.export"
+$geomesa export -u root -p secret -c test1 -f re-tsv-hdfs   -F csv  | sort >  "$t/re.tsv.hdfs.export"
+
+target="$t/e.csv.sorted"
+cat "$t/e.csv" | sort > "$target"
 
 
+function differ() {
+  local f=$1
+  echo "diff $f"
+  diff "${target}" "$f"
+  echo "done"
+}
 
+differ "$t/re.avro.export"
+differ "$t/re.avro.hdfs.export"
+differ "$t/re.csv.export"
+differ "$t/re.csv.hdfs.export"
+differ "$t/re.tsv.export"
+differ "$t/re.tsv.hdfs.export"
 echo "DONE"
